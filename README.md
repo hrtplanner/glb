6cd4929e8e2bcb7352e147376177580405a34049
alls <alls@arch>
2025-02-27 12:46:27 -0500

    Update README to explain the website

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Pardon me while I clean up this code...
+# Gender Lego Bricks
+
+Visual-spatial representation of gender identity corresponding to a questionnaire.
+
+### What?
+
+Gender Lego Bricks is a website and questionnaire format that shows a basic (not too complex) rundown of gender identity with a short, sharable link. Further information is explained directly ON the website.   
+Each instance is intended to be customized (down to the explanation) to improve upon the base features of GLB, and provide more advanced properties (such as genderfluidity or other identities which are not included by default).
+
+### Questionnaire Format
+
+There are four multiple-choice questions that aim to build your brick (collection of properties).   
+- Gender identity: Determines "core groups" and main pronouns. Core groups include masculine, feminine, non-binary and agender. They can be rearranged to show pronoun precedence.
+- Bigender identity: Determines if the user is bigender (has mixed properties of selected genders or fluctuates between them).
+- Etc. pronoun choices: Determines if the user uses pronouns other than the main ones, or, collectively, any other pronoun.
+- Gender expression: Determines main, a mix of, or variable properties of the user's appearance and behavior.
+
+### Credits
+
+This project was heavily inspired by the [Attraction Layer Cake](https://cake.avris.it/), down to the specific design.   
+[Allie](https://crit.rip) developed the project.
+
+### Access
+
+To view the project live (limit of 100,000-or-so visits/day due to Cloudflare Pages), visit [Allie's instance](https://glb.crit.rip).   
+It is preferable to modify GLB to fit your needs and deploy it yourself if any more resources are necessary.
--- src/tutorial/Popup.svelte
+++ src/tutorial/Popup.svelte
@@ -17,52 +17,50 @@
 </script>
 
 <div>
-    <h1>GLB Organization</h1>
+    <h1>Gender Lego Bricks Organization</h1>
     <p>I'm Allie, the developer of GLB. <a href="/#FLER" target="_blank">This is my brick</a>.</p>
-    <p>Gender identity specified via GLB are organized into "bricks," which visualize certain data.</p>
+    <p>GLB shows your gender identity as "bricks" that display different aspects of who you are.</p>
     <img src={Step1} title={language[0]} alt={language[0]} />
-    <p>This brick, overall, represents an agender gender identity, preferring agender pronouns, accepting any other pronouns and fluctuating between a more masculine identity.</p>
-    <i>But don't get overwhelmed, it's (mostly) simple.</i>
+    <p>This example brick shows an agender identity that prefers agender pronouns, accepts any pronouns, and sometimes feels more masculine.</p>
+    <i>Don't worry, though, it's simpler than it looks.</i>
 
-    <h2>Core Groups</h2>
-    <p>The four main organizations of bricks are <i>masculine, feminine, non-binary and agender</i>.</p>
-    <p>Masculine represents a male-adjacent gender identity, feminine represents a female-adjacent gender identity, non-binary represents an identity outside the two and agender represents "none-of-the-above."</p>
+    <h2>The Four Core Groups</h2>
+    <p>Bricks are organized into four main groups: <i>masculine, feminine, non-binary and agender</i>.</p>
+    <p>Masculine means male-adjacent, feminine means female-adjacent, non-binary means outside these two, and agender means "none of these."</p>
 
-    <h3>Gender identity</h3>
-    <p>Gender identity is the bottom-most, "base," layer of each core group. They are toggled on-and-off by both the gender expression/presentation and gender identity questionnaires.</p>
-    <p>Enabling an agender gender identity from the first questionnaire will also enable the non-binary gender identity block in response.</p>
-    <p>The "gender identity" block determines the "base" stereotypical pronouns, the pronoun of which will be enabled/disabled in response.</p>
+    <h3>Gender Identity (Base Layer)</h3>
+    <p>The bottom layer shows your basic gender identity. This comes from your answers in the questionnaires.</p>
+    <p>Choosing agender will automatically include non-binary too.</p>
+    <p>This base layer also sets up your default pronouns.</p>
     <img src={Step2} title={language[1]} alt={language[1]} />
 
-    <h3>Pronoun precedence</h3>
-    <p>Pronoun precedence is the second-most layer of each core group.</p>
-    <p>Rearranging the order of gender identities present in the first question will affect the order of the "base" pronouns in the stereotypical pronouns generated.</p>
-    <p>It will also change the precedence (depth from front-to-back) of the pronoun assigned to the gender identity.</p>
-    <p>Pronoun precedence defines which pronouns you prefer, and in what order. For example, they/he/she defines that "they" is preferred, "he" is a good second, and "she" is just okay.</p>
-    <p>Pronoun precedence does not apply to "any" or "other" pronouns, if selected. They will go last.</p>
+    <h3>Pronoun Order</h3>
+    <p>The second layer shows which pronouns you prefer most.</p>
+    <p>The order matters! For example, "they/he/she" means you prefer "they" first, "he" second, and "she" third.</p>
+    <p>The order comes from how you arrange your gender identities in the first question.</p>
+    <p>"Any" or "other" pronouns always go last if selected.</p>
     <img src={Step3} title={language[2]} alt={language[2]} />
 
-    <h3>Gender expression</h3>
-    <p>Gender expression is the third-most layer of each core group, which can be modified by the fourth question in the questionnaire.</p>
-    <p>Enabling a gender expression will, in return, enable a base block of gender identity: NOT a pronoun, or obviously expressed identity. Enabling agender identity, however, will enable non-binary in the same fashion as the gender identity questionnaire.</p>
-    <p>Gender expression describes how the user (you) presents themselves in appearance, which corresponds with the core group.</p>
-    <p><b>These can be mixed.</b> Mixing gender expressions represents a fluctuating appearance, similar to bigender identity.</p>
+    <h3>Gender Expression</h3>
+    <p>The third layer shows how you present yourself to others.</p>
+    <p>This comes from the fourth question in the survey.</p>
+    <p>Your expression choices activate matching identity blocks in the base layer.</p>
+    <p><b>You can mix expressions</b>: Mixed expressions show that your appearance has multiple properties of either, varies or fluctuates (similar to bigender identity).</p>
     <img src={Step4} title={language[3]} alt={language[3]} />
 
-    <h3>Bigender identity</h3>
-    <p>Bigender identity is the fourth-most layer of each core group.</p>
-    <p>Bigender identity represents a mixed (or even fluctuating) gender identity that may not specifically correspond with each identity.</p>
-    <p>It is enabled by selecting one or more options in the second question. It may be combined with a base option if the user has a constant identity along with a mixed or fluctuating identity.</p>
-    <p>It is not required, but it is an option for those who are bigender.</p>
-    <p>Bigender identities come last in the stereotypical pronoun set, but are denoted with a "+" (plus sign) beforehand to show mixing or fluctuation between those and base identities.</p>
+    <h3>Bigender Identity</h3>
+    <p>The fourth layer shows if you have mixed or fluid gender identities.</p>
+    <p>This comes from your answers to the second question.</p>
+    <p>You can combine this with your base identity if you have both constant and fluid aspects.</p>
+    <p>Bigender identities appear in your pronoun set with a "+" sign (like "she/they/+he").</p>
     <img src={Step5} title={language[4]} alt={language[4]} />
 
-    <h2>Pronoun options</h2>
-    <p>Belonging to the third question in the questionnaire, pronoun options give the user (you) a verbose method of expressing if they use all other pronouns, a specific set of other pronouns (such as neopronouns), none, or both.</p>
-    <p>Pronoun options have the back-most layer, which covers the entire brick in width and height. The layer is split into two sections: bottom-most for all other pronouns, top-most for neopronouns or a specific other set.</p>
+    <h2>Pronoun Options</h2>
+    <p>The back layer (from the third question) shows if you use all pronouns, specific pronouns like neopronouns, or both.</p>
+    <p>This layer covers the entire brick and has two parts: bottom section for all pronouns, top section for specific others.</p>
     <img src={Step6} title={language[5]} alt={language[5]} />
 
-    <p style="margin-top: 30px;">I hope this is a useful tool for you when discovering or sharing your gender identity.</p>
+    <p style="margin: 30px;">I hope this helps you discover and share your gender identity!</p>
 </div>
 
 <style scoped>
